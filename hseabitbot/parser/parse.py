6cb2b8314af2e4b73630afe6d54c60bb991af323
playerr17 <97340151+playerr17@users.noreply.github.com>
2023-07-03 13:34:18 +0500

    add parser

--- hseabitbot/parser/parse.py
+++ hseabitbot/parser/parse.py
@@ -1,10 +1,15 @@
 import asyncio
 import numpy
+import numpy as np
 
 
 async def replace_with_ones(table: numpy.ndarray) -> None:
     table[(table == 'Да') | (table == 'Yes')] = 1
     table[(table == 'Нет') | (table == 'No')] = 0
+
+
+async def find_cell_with(table: numpy.ndarray, value: [int, str]) -> [int, int]:
+    return numpy.argwhere(table == value)[0]
 
 
 async def main(table: numpy.ndarray) -> str:
@@ -13,8 +18,9 @@ async def main(table: numpy.ndarray) -> str:
     {table[8, 2]}
     Бюджетных мест: *{table[5, 10]}*
     Квазибюджетных мест: *{table[6, 10]}*
-    Подано заявлений БВИ: *{numpy.sum(table[16:, 4])}* (подали согласие: *{counter_sogl_bvi}*)
-    Подано заявлений по особой квоте(не более {info[4]}): *{numpy.sum(table[16:, 6])}*
-    Подано заявлений по целевой квоте(не более {info[5]}): *{numpy.sum(table[16:, 8])}*
-    Подано заявлений по специальной квоте(не более {info[6]}): *{numpy.sum(table[16:, 10])}*
+    Подано заявлений БВИ: *{numpy.sum(table[16:, 4])}* (подали согласие: *{numpy.sum(table[16:, 4] *
+                                                        table[16:, find_cell_with(table, 'Оригинал аттестата')[1]])}*)
+    Подано заявлений по особой квоте: *{numpy.sum(table[16:, 6])}*
+    Подано заявлений по целевой квоте: *{numpy.sum(table[16:, 8])}*
+    Подано заявлений по специальной квоте: *{numpy.sum(table[16:, 10])}*
     """
