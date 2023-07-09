2603b1c4b12478f6d8e69e7580d4875fb3685718
playerr17 <97340151+playerr17@users.noreply.github.com>
2023-07-10 00:09:01 +0300

    Add inline commands

--- hseabitbot/inline_commands/inline_mode.py
+++ hseabitbot/inline_commands/inline_mode.py
@@ -1,21 +1,25 @@
 import asyncio
 import aiogram
 
+from utils import config
+
 
 async def answer_inline_query(inline_query: aiogram.types.InlineQuery) -> None:
-    text = inline_query.query + '1'
+    text = inline_query.query.lower()
     results = []
-    results.append(aiogram.types.InlineQueryResultArticle(
-        id=link,  # ссылки у нас уникальные, потому проблем не будет
-        title=inline_query.query,
-        description=link_data["description"],
-        input_message_content=InputTextMessageContent(
-            message_text=get_message_text(
-                link=link,
-                title=link_data["title"],
-                description=link_data["description"]
-            ),
-            parse_mode="HTML"
-        )
-    ))
-    await inline_query.answer(text, is_personal=True)
+    event_id = int(inline_query.id)
+    counter = 0
+    for program_id, program_name in zip(config.bachelor_programs, config.program_names):
+        if text in program_name.lower():
+            counter += 1
+            results.append(aiogram.types.InlineQueryResultArticle(
+                id=str(event_id + counter),
+                title=program_name,
+                input_message_content=aiogram.types.InputTextMessageContent(
+                    message_text=program_id,
+                    parse_mode="HTML"
+                )
+            ))
+        if counter >= 10:
+            break
+    await inline_query.answer(results[:20], is_personal=True)
--- hseabitbot/user/__init__.py
+++ hseabitbot/user/__init__.py
@@ -1,60 +1,11 @@
 import sqlite3
+import sys
 
-bachelor_programs = [
-    'BD_Akter',
-    'BD_Ant_Ist',
-    'BD_Ant_Fil',
-    'BD_Bible',
-    'BD_BI',
-    'BD_Vostok',
-    'BD_GGIGT',
-    'BD_ZHur',
-    'BD_InYaz',
-    'BD_ITSS',
-    'BD_IVT',
-    'BD_IB',
-    'BD_ISTR',
-    'BD_Isk',
-    'BD_Film',
-    'BD_CMB',
-    'BD_KogNeir',
-    'BD_Compds',
-    'BD_Cultural',
-    'BD_Marketing',
-    'BD_Math',
-    'BD_Media',
-    'BD_icef',
-    'BD_MO',
-    'BD_MB',
-    'BD_WE',
-    'BD_Moda',
-    'BD_Political',
-    'BD_Pravo',
-    'BD_AM',
-    'BD_AMI',
-    'BD_Data',
-    'BD_SE',
-    'BD_Psy',
-    'BD_AD',
-    'BD_CPM',
-    'BD_Art',
-    'BD_Soc',
-    'BD_Producer',
-    'BD_bba',
-    'BD_Creative',
-    'BD_Logistics',
-    'BD_Physics',
-    'BD_Philology',
-    'BD_Phil',
-    'BD_Ling',
-    'BD_Chem',
-    'BD_dlawyer',
-    'BD_digital',
-    'BD_Stat',
-    'BD_EA',
-    'BD_Iran',
-    'BD_India'
-]
+sys.path.append("../")
+
+from utils import config
+
+bachelor_programs = config.bachelor_programs
 
 
 def __init__():
