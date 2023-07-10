c16af968af75ab0ab6f8162f76eab9c036823c62
playerr17 <97340151+playerr17@users.noreply.github.com>
2023-07-10 14:13:38 +0300

    fix ImportError

--- hseabitbot/downloader/__init__.py
+++ hseabitbot/downloader/__init__.py
@@ -1,7 +1,7 @@
-from .download import bachelor_programs
+from utils import config
 from .download import file_hashes
 
 
 def __init__() -> None:
-    for program in bachelor_programs:
+    for program in config.bachelor_programs:
         file_hashes[program] = ''
