7d407341bf460297ffdb523268cc8e4668c440b9
playerr17 <97340151+playerr17@users.noreply.github.com>
2023-07-12 22:10:48 +0300

    fix KeyError and path

--- hseabitbot/db_commands/get_info.py
+++ hseabitbot/db_commands/get_info.py
@@ -5,7 +5,7 @@ from . import connect
 
 async def get_user_list(program: str) -> [int]:
     table_query = f"""SELECT * from {program}"""
-    cursor, connection = await connect.make_connection("user/sending.db")
+    cursor, connection = await connect.make_connection("hseabitbot/user/sending.db")
     try:
         cursor.execute(table_query)
         content = cursor.fetchall()
